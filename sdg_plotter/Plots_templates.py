9115ccfae6a4ad401df481e2a421a4087c6f783e
maronmi <michele.maroni@ext.ec.europa.eu>
2021-05-05 16:29:05 +0200

    functionalized barplot template for SDG mapping

--- sdg_plotter/Plots_templates.py
+++ sdg_plotter/Plots_templates.py
@@ -69,47 +69,54 @@ sdg_color = {label : color for label, color in zip(df['Name'].tolist(),color_ls)
 
 
 
+sdg_dict = {'whole':[8,205,46,8,2,105,17,97,282,2,18,109,233,89,210,23,31],
+'jrc':[79,436,234,231,19,171,329,706,1959,60,427,450,996,104,230,343,106],
+'f2f':[51,6,45,166,33,16,126,126,49,37,58,10,109,3,7,46,9]
+}
+whole=[8,205,46,8,2,105,17,97,282,2,18,109,233,89,210,23,31]
+jrc=[79,436,234,231,19,171,329,706,1959,60,427,450,996,104,230,343,106]
+f2f=[51,6,45,166,33,16,126,126,49,37,58,10,109,3,7,46,9]
 
-# whole=[8,205,46,8,2,105,17,97,282,2,18,109,233,89,210,23,31]
-# jrc=[79,436,234,231,19,171,329,706,1959,60,427,450,996,104,230,343,106]
-
-f2f=[1,88,7,0,0,3,7,5,12,1,0,42,23,8,15,0,8]
-
-
-whole=[0,2,0,8,0,1,10,18,124,1,3,48,80,8,6,5,8]
-rd_sl_rrp=[0,1,0,3,0,0,6,12,88,1,2,17,35,0,0,5,4]
-rd_sl_rrp_annex_EN=[0,1,0,5,0,1,4,6,36,0,1,31,45,8,6,0,4]
 
 #####Barplot#####
 # Importing the matplotlib library
 import numpy as np
 import matplotlib.pyplot as plt
-# Declaring the figure or the plot (y, x) or (width, height)
-plt.figure(figsize=[15, 10])
-# Data to be plotted
-#Rrp = [random.randint(1, 400) for i in range(1,18,1)]
-#RecPlan = [random.randint(1, 400) for i in range(1,18,1)]
-#EUSem = [random.randint(1, 400) for i in range(1,18,1)]
-# # Using numpy to group 3 different data with bars
-X = np.arange(len(whole))
-# # Passing the parameters to the bar function, this is the main function which creates the bar plot
-# # Using X now to align the bars side by side
-#plt.bar(X, whole, color = sdg_color.values(), alpha = 1, width = 0.75)    
-plt.bar(X, f2f, color = sdg_color.values(), alpha = 0.95, width = 0.5)
-#plt.bar(X + 0.5, rd_sl_rrp_annex_EN, color = sdg_color.values(), alpha = 0.75, width = 0.5)
-# Creating the legend of the bars in the plot
-#plt.legend(['maindoc+annex', 'rd_sl_rrp', 'rd_sl_rrp_annex'])
-# Overiding the x axis with the country names
-plt.xticks([i for i in range(0,17,1)], df['Name'].tolist(), fontsize='large', fontweight='bold', rotation=45)
-plt.yticks(fontsize='large', fontweight='bold')
-# Giving the tilte for the plot
-plt.title("Mentions", fontsize='large', fontweight='bold') #within {path.name}
-# Namimg the x and y axis
-#plt.xlabel('Countries')
-plt.ylabel('Counts',fontsize='large', fontweight='bold')
-plt.savefig('f2f.svg', dpi=600)
-# Saving the plot as a 'png'
-# if save_plot=True:
-#     plt.savefig(svg_filepath_variable)
-# Displaying the bar plot
-plt.show()
+def sdg_barlpot(sdg_item, outfile=None):
+
+    
+    params_dict={'resolution':[15, 10],'plot_title' : '',
+    'alpha':1, 'width' : 1,
+    'xtick_labels' : [f'SDG {count}' for count in range(1,18,1)],'y_label': 'Count'}
+    #if params_dict == None: if params dict diff none update keys/values with passed dictionary else use standard
+
+    # Declaring the figure or the plot (y, x) or (width, height)
+    fig = plt.figure(figsize=params_dict['resolution'])
+
+    # Data to be plotted
+    X = np.arange(17)
+
+    if isinstance(sdg_item, list):  
+        plt.bar(X, sdg_item, color = sdg_color.values(), alpha = params_dict['alpha'], width = params_dict['width'])
+    
+    elif isinstance(sdg_item,dict):
+        for counter, key in enumerate(sdg_item.keys()):
+            plt.bar(X+counter*params_dict['width']/(len(sdg_item)), 
+            sdg_item[key], color = sdg_color.values(), 
+            alpha = params_dict['alpha']-(counter*params_dict['alpha']/(len(sdg_item))/1.5), 
+            width = params_dict['width']/(len(sdg_item)))
+            plt.legend(list(sdg_item.keys()))
+
+   
+    plt.xticks([i for i in range(0,17,1)], params_dict['xtick_labels'], fontsize='large', fontweight='bold', rotation=45)
+    plt.yticks(fontsize='large', fontweight='bold')
+    
+    plt.title(params_dict['plot_title'], fontsize='large', fontweight='bold') #within {path.name}
+    
+    # plt.xlabel('SDGs')
+    plt.ylabel(params_dict['y_label'], fontsize='large', fontweight='bold')
+    if outfile != None:
+        plt.savefig(f'{outfile}', dpi=600)
+    return fig
+
+sdg_barlpot([random.randint(1, 400) for i in range(1,18,1)], outfile='test.png')
